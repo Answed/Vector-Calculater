2e3b033360f2523a2962a0828f88311c95e4c00a
Answed <schradearwed@gmail.com>
2023-03-09 17:25:01 +0100

    Removed the old angle method

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -1,7 +1,3 @@
 from VectorClasses import *
 import math as mt
 
-def angle_between_vectors(vec1, vec2):
-    scalar_product = vec1.scalar_product(vec2)
-    angle = mt.acos(scalar_product/(vec1.length() * vec2.length()))
-    return mt.degrees(angle)
