b06d4db0c93c7c75a802a107ed2a65cf35aaa903
Answed <schradearwed@gmail.com>
2023-03-11 15:00:37 +0100

    Fixed Vector 3 Operations Fixed point_on_straight Added distance_between_straights

--- VectorClasses.py
+++ VectorClasses.py
@@ -44,13 +44,13 @@ class Vector3(Vector2):
     
     # Function overrides which are requierd for vector math operations
     def __add__(self, other): # Allows that two Vector3s can be added to each other. Resulting Vector is a new Vector3 to ensure that no data gets corupted and easy working with different Vectors multiple times.
-        return Vector2(self.x + other.x, self.y + other.y, self.z + other.z)
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
     def __sub__(self, other): # Allows that two Vector2s can be subtracted from each other
-        return Vector2(self.x - other.x, self.y - other.y, self.z - other.z) 
+        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z) 
     def __mul__(self, other): # Allows to multiply a Vector3 with floats or intigers
-        return Vector2(self.x * other, self.y * other, self.z * other)
+        return Vector3(self.x * other, self.y * other, self.z * other)
     def __truediv__(self, other): # Allows to divide a Vector3 with floats or intigers
-        return Vector2(self.x / other, self.y / other, self.z / other)
+        return Vector3(self.x / other, self.y / other, self.z / other)
     def __str__(self):
         return "({} | {} | {})".format(self.x, self.y, self.z)
 
@@ -59,43 +59,44 @@ class Vector3(Vector2):
         return mt.sqrt(self.x**2 + self.y**2 + self.z**2)
     def normalize(self): # Calculates the normalized version of a Vector3
         lenghtOfVector = self.length()
-        return Vector2(self.x / lenghtOfVector, self.y / lenghtOfVector, self.z / lenghtOfVector)
+        return Vector3(self.x / lenghtOfVector, self.y / lenghtOfVector, self.z / lenghtOfVector)
     
     # Calculations where another vector is required for 
     def scalar_product(self, other): # Allows to calculate the skalar from two vecotor3's 
         return((self.x * other.x )+ (self.y * other.y) + (self.z * other.z))
     
     def cross_product(self, other): # Creates a new Vector out of the old two. 
-        new_x = self-y * other.z - self.z * other.y
+        new_x = self.y * other.z - self.z * other.y
         new_y = self.z * other.x - self.x * other.z
         new_z = self.x * other.y - self.y * other.x
         return Vector3(new_x, new_y, new_z)
     
-    class Straight2D:
-        def __init__(self, base_point, direction) -> None:
-            self.base_point = base_point
-            self.direction = direction
+class Straight2D:
+    def __init__(self, base_point, direction) -> None:
+        self.base_point = base_point
+        self.direction = direction
         
-        def point_on_straight(self, multiplier):
-            return Vector2(self.base_point + multiplier * self.direction)
-        
-        def convert_straight_to_equation(self):
-            x_equation = "{}+{}x".format(self.base_point.x, self.direction.x)
-            y_equation = "{}+{}x".format(self.base_point.y, self.direction.y)
-            return [x_equation, y_equation]
+    def point_on_straight(self, multiplier):
+        temp_vektor = self.direction * multiplier
+        return self.base_point + temp_vektor
 
-        def distance_between_straights(self, other):
-            helparea = self.direction.scalar_product(other.base_point) # Calculates the vale of the helparea which is needed for the calculation of the point from the straight
-        
+    def convert_straight_to_equation(self):
+        x_equation = "{}+{}x".format(self.base_point.x, self.direction.x)
+        y_equation = "{}+{}x".format(self.base_point.y, self.direction.y)
 
+    def distance_between_straights(self, other):
+        helparea = self.direction.scalar_product(other.base_point) # Calculates the vale of the helparea which is needed for the calculation of the point from the straight
+        number_without_parameter = self.direction.scalar_product(self.base_point)
+        helparea -= number_without_parameter
+        number_with_parameter = self.direction.scalar_product(self.direction)
+        parameter = helparea / number_with_parameter
+        distance_vektor = self.point_on_straight(parameter) - other.base_point
+        return distance_vektor.length()
     
-    class Straight3D(Straight2D):
-                
-        def point_on_straight(self, multiplier):
-            return Vector3(self.base_point + multiplier * self.direction)
+class Straight3D(Straight2D):
         
-        def convert_straight_to_equation(self):
-            x_equation = "{}+{}x".format(self.base_point.x, self.direction.x)
-            y_equation = "{}+{}x".format(self.base_point.y, self.direction.y)
-            z_equation = "{}+{}x".format(self.base_point.z, self.direction.z)
-            return [x_equation, y_equation, z_equation]
+    def convert_straight_to_equation(self):
+        x_equation = "{}+{}x".format(self.base_point.x, self.direction.x)
+        y_equation = "{}+{}x".format(self.base_point.y, self.direction.y)
+        z_equation = "{}+{}x".format(self.base_point.z, self.direction.z)
+        return [x_equation, y_equation, z_equation]
