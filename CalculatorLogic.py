b9b5b5f1edcac5a02ab25390aca254be98282614
Answed <schradearwed@gmail.com>
2023-03-12 15:23:00 +0100

    Just the test file dont bother

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -2,3 +2,8 @@ from VectorClasses import *
 import math as mt
 
 
+g = Straight3D(Vector3(2, 1, 3), Vector3(1, 2, 3))
+h = Straight3D(Vector3(1, 3, 4), Vector3(3, 1, 2))
+
+print(g.distance_between_straights(h))
+
