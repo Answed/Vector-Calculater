816396094a0c5da9798d05b898b9e48cfe4c69ae
Answed <schradearwed@gmail.com>
2023-03-17 18:26:53 +0100

    Added generate_straight2d

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -1,2 +1,13 @@
-from VectorClasses import *
+from VectorClasses import*
 import math as mt
+import matplotlib.pyplot as plt
+import numpy as np
+
+def generate_straight2d(straight: Straight2D):
+    x = []
+    y = []
+    for i in range(100): # Will be changeable later in development
+        new_point = straight.point_on_straight(i)
+        x.append(new_point.x)
+        y.append(new_point.y)
+    return [x, y]
