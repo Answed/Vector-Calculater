5633119e3913ab4d869cabb1f56931f97f995e1c
Answed <schradearwed@gmail.com>
2023-03-17 18:28:33 +0100

    added generate_straight3d

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -11,3 +11,14 @@ def generate_straight2d(straight: Straight2D):
         x.append(new_point.x)
         y.append(new_point.y)
     return [x, y]
+
+def generate_straight3d(straight: Straight3D):
+    x = []
+    y = []
+    z = []
+    for i in range(100): # Will be changeable later in development
+        new_point = straight.point_on_straight(i)
+        x.append(new_point.x)
+        y.append(new_point.y)
+        z.append(new_point.z)
+    return [x, y, z]
