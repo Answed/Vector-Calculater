a5bf42eab6798864cef3cc9b6387fe84a8ccd8ee
Answed <schradearwed@gmail.com>
2023-03-17 18:27:07 +0100

    Added Tab control

--- UserInterface.py
+++ UserInterface.py
@@ -1,10 +1,17 @@
 import tkinter as tk
+from tkinter import ttk
 from VectorClasses import*
-import matplotlib.pyplot as plt
+import CalculatorLogic as Cl
 
 main_Frame = tk.Tk()
 main_Frame.title("Vector Calculator")
 main_Frame.geometry("480x270")
 
+tabcontrol = ttk.Notebook(main_Frame)
+
+main_tab = ttk.Frame(tabcontrol)
+
 if __name__ == "__main__":
     main_Frame.mainloop()
+
+
