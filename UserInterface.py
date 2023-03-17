cd25c4191b39d0c4398591024292a29ac8baa23e
Answed <schradearwed@gmail.com>
2023-03-17 17:54:13 +0100

    Added base window to UserInterface.py Cleaned CalculatorLogic.py

--- UserInterface.py
+++ UserInterface.py
@@ -2,4 +2,9 @@ import tkinter as tk
 from VectorClasses import*
 import matplotlib.pyplot as plt
 
+main_Frame = tk.Tk()
+main_Frame.title("Vector Calculator")
+main_Frame.geometry("480x270")
 
+if __name__ == "__main__":
+    main_Frame.mainloop()
