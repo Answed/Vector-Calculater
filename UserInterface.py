60ff7552ccc47fbf823a710b66b5723b3e1b56f5
Answed <schradearwed@gmail.com>
2023-03-25 15:33:39 +0100

    Added Vector 3 UI Elements

--- UserInterface.py
+++ UserInterface.py
@@ -18,10 +18,17 @@ def MainTab():
     new_vector2_E = tk.Entry(main_tab, width=50)
     add_vector2_B = tk.Button(main_tab, text="Add Vector")
 
+    new_vector3_L = tk.Label(main_tab, text="Create a new Vector 3")
+    new_vector3_E = tk.Entry(main_tab, width=50)
+    add_vector3_B = tk.Button(main_tab, text="Add Vector")
+
+
     new_vector2_L.grid(column=0, row=0)
     new_vector2_E.grid(column=1, row=0)
     add_vector2_B.grid(column=1, row=1)
-
+    new_vector3_L.grid(column=0, row=2)
+    new_vector3_E.grid(column=1, row=2)
+    add_vector3_B.grid(column=1, row=3)
 
 if __name__ == "__main__":
     MainTab()
