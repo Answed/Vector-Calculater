be28b68e07e7ae27507241919f6e0e024935ef77
Answed <schradearwed@gmail.com>
2023-03-27 16:29:49 +0200

    Added Node Class for Binary Tree

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -5,6 +5,28 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import DataManager as DM
 
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+    
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+             self.data = data
+
+
 def generate_straight2d(straight: Straight2D):
     x = []
     y = []
