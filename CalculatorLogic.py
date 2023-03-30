e79b3536f820230b682a24a2a4f31dcf1db8bb10
Answed <schradearwed@gmail.com>
2023-03-30 19:23:02 +0200

    Removed the node class

--- CalculatorLogic.py
+++ CalculatorLogic.py
@@ -5,26 +5,6 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import DataManager as DM
 
-class Node:
-    def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
-    
-    def insert(self, data):
-        if self.data:
-            if data < self.data:
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.left.insert(data)
-            elif data > self.data:
-                if self.right is None:
-                    self.right = Node(data)
-                else:
-                    self.right.insert(data)
-        else:
-             self.data = data
 
 def generate_vector2D(vector: Vector2):
     lenght = vector.length()
