eabeb1fb4549dce4ca01ecfb9c292f5803a72fba
Answed <schradearwed@gmail.com>
2023-04-27 09:14:22 +0200

    Fixed calculator layout

--- UserInterface.py
+++ UserInterface.py
@@ -99,7 +99,7 @@ class Calculator(tk.Frame):
         distance_between_straights_E.insert(0, "straight1 - straight2")
         calculate_distance_B = tk.Button(self, text="Calculate distance", command=lambda: Cl.calculate_distance(distance_between_straights_E.get()))
 
-        point_on_area_L = tk.Label(self, text="Calculates if a point is on a straight or area\nEnter the name of the Area or Straight and the vector as name or use (x|y)...")
+        point_on_area_L = tk.Label(self, text="Calculates if a point is on a straight or area\nEnter the name of the Area or Straight\n and the vector as name or use (x|y)...")
         point_on_area_E = tk.Entry(self, width=25)
         point_on_area_E.insert(0, "NameOfArea/Straight + point")
         check_if_point_is_in_it = tk.Button(self, text="Check if point\n is in it", command=lambda: Cl.check_point(point_on_area_E.get()))
